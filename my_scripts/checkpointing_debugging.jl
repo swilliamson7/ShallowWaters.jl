86aefe05148100c355584dca73a27bef7cd59f9e
Sarah Williamson <swilliamson@utexas.edu>
2024-02-14 12:10:03 -0600

    Enzyme not working
    Previously working with this code, now throwing an error

--- my_scripts/checkpointing_debugging.jl
+++ my_scripts/checkpointing_debugging.jl
@@ -396,7 +396,7 @@ end
 # working (fingers crossed)
 function run_checkpointing()
 
-    S = ShallowWaters.run_setup(nx = 50,
+    S = ShallowWaters.run_setup(nx = 30,
     Ndays = 1,
     zb_forcing_momentum=false,
     zb_filtered=false,
@@ -407,48 +407,17 @@ function run_checkpointing()
 
     dS = Enzyme.Compiler.make_zero(Core.Typeof(S), IdDict(), S)
     snaps = Int(floor(sqrt(S.grid.nt)))
-    revolve = Revolve{ShallowWaters.ModelSetup}(S.grid.nt, snaps; verbose=1, gc=true, write_checkpoints=false)
-
-    autodiff(Enzyme.ReverseWithPrimal, checkpoint_function, Duplicated(S, dS), revolve)
-
-    return S, dS
-
-end
-
-# not working
-function run_energy_checkpointing()
-
-    energy_high_resolution = load_object("data_files_gamma0.3/512_post_spinup_4years/energy_post_spinup_512_4years_012524.jld2")
-    grid_scale = 4
-
-    # aiming to have data about every 30 days
-    data_steps = 6733:6733
-    data = [energy_high_resolution[6733*grid_scale]]
-
-    S = ShallowWaters.run_setup(nx = 50,
-        Ndays = 30,
-        zb_forcing_dissipation=true,
-        zb_filtered=true,
-        data_steps=data_steps,
-        data=data,
-        initial_cond="ncfile",
-        initpath="./data_files_gamma0.3/128_spinup_wforcing_dissipation_wfilter_1pass/"
-    )
-
-    dS = Enzyme.Compiler.make_zero(Core.Typeof(S), IdDict(), S)
-
-    snaps = Int(floor(sqrt(S.grid.nt)))
     revolve = Revolve{ShallowWaters.ModelSetup}(S.grid.nt,
-        snaps;
-        verbose=1,
-        gc=true,
-        write_checkpoints=false
+    snaps;
+    verbose=1,
+    gc=true,
+    write_checkpoints=false
     )
 
     autodiff(Enzyme.ReverseWithPrimal,
-        checkpoint_function,
-        Duplicated(S, dS),
-        revolve
+    checkpoint_function,
+    Duplicated(S, dS),
+    revolve
     )
 
     return S, dS
