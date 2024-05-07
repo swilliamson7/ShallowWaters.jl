a2e39223b9ee3a62beda739b5245fbd5fe76fe99
Sarah Williamson <swilliamson@utexas.edu>
2024-05-07 16:14:42 -0500

    Removing my_scripts
    Nothing in this folder contains what I want to add to ShallowWaters.jl

--- src/run_model.jl
+++ src/run_model.jl
@@ -45,41 +45,6 @@ function run_model(::Type{T},P::Parameter) where {T<:AbstractFloat}
 
 end
 
-#### for checking that the modified time_integration still works ##################################
-
-function run_check(::Type{T}=Float32;     # number format
-    kwargs...                             # all additional parameters
-    ) where {T<:AbstractFloat}
-
-    P = Parameter(T=T;kwargs...)
-    return run_check(T,P)
-end
-
-function run_check(P::Parameter)
-    @unpack T = P
-    return run_check(T,P)
-end
-
-function run_check(::Type{T},P::Parameter) where {T<:AbstractFloat}
-
-    @unpack Tprog = P
-
-    G = Grid{T,Tprog}(P)
-    C = Constants{T,Tprog}(P,G)
-    F = Forcing{T}(P,G)
-    # S = ModelSetup{T,Tprog}(P,G,C,F)
-
-    Prog = initial_conditions(Tprog,G,P,C)
-    Diag = preallocate(T,Tprog,G)
-
-    # one structure with everything already inside
-    S = ModelSetup{T,Tprog}(P,G,C,F,Prog,Diag)
-    P = time_integration_mine(S)
-
-    return P
-
-end
-
 ##################################################################
 
 ## Built in Enzyme run #######################################################################
